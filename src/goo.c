5c7f550802513e5382569fc2f65f727d8bdb35b5
serdonke <github@serdonke.xyz>
2025-03-19 10:45:39 +0530

    I am made of stupid...

--- src/goo.c
+++ src/goo.c
@@ -2,6 +2,7 @@
 #define GOO_C_
 
 #include <stddef.h>
+#include <stdio.h>
 #include <stdint.h>
 #include <math.h>
 
@@ -12,50 +13,59 @@ typedef struct Color {
     uint8_t r;
     uint8_t g;
     uint8_t b;
+    uint8_t a;
 } Color;
+
+Color GRUVBOX_BG      = {  40,  40,  40, 255 };  // Background
+Color GRUVBOX_FG      = { 235, 219, 178, 255 };  // Foreground
+Color GRUVBOX_RED     = { 204,  36,  29, 255 };  // Red
+Color GRUVBOX_GREEN   = { 152, 151,  26, 255 };  // Green
+Color GRUVBOX_YELLOW  = { 215, 153,  33, 255 };  // Yellow
+Color GRUVBOX_BLUE    = {  69, 133, 136, 255 };  // Blue
+Color GRUVBOX_PURPLE  = { 177,  98, 134, 255 };  // Purple
+Color GRUVBOX_AQUA    = { 104, 157, 106, 255 };  // Aqua
+Color GRUVBOX_ORANGE  = { 214,  93,  14, 255 };  // Orange
 
 int8_t gooFill(uint8_t* surface, Color color)
 {
     for (size_t i = 0; i < HEIGHT * WIDTH; i++)
     {
-        surface[i * 3 + 0] = color.r;
-        surface[i * 3 + 1] = color.g;
-        surface[i * 3 + 2] = color.b;
+        surface[i * 4 + 0] = color.r;
+        surface[i * 4 + 1] = color.g;
+        surface[i * 4 + 2] = color.b;
+        surface[i * 4 + 3] = color.a;
     }
     return 0;
 }
 
 // Convert (x, y) coordinate into 1D stride based coordinate
-uint32_t gooParsePixel(uint32_t x, uint32_t y)
+static inline uint32_t gooParsePixel(int32_t x, int32_t y)
 {
     return y * WIDTH + x;
 }
 
-void gooPutPixel(uint8_t* surface, uint32_t x, uint32_t y, Color color)
+static inline void gooPutPixel(uint8_t* surface, int32_t x, int32_t y, Color color)
 {
-    surface[gooParsePixel(x, y) * 3 + 0] = color.r;
-    surface[gooParsePixel(x, y) * 3 + 1] = color.g;
-    surface[gooParsePixel(x, y) * 3 + 2] = color.b;
+    uint32_t index = gooParsePixel(x, y) * 4;
+    surface[index + 0] = color.r;
+    surface[index + 1] = color.g;
+    surface[index + 2] = color.b;
+    surface[index + 3] = color.a;
 }
 
-void gooDrawLineNaive(uint8_t* surface, uint32_t x0, uint32_t y0, uint32_t x1, uint32_t y1, Color color)
+void gooDrawLineNaive(uint8_t* surface, int32_t x0, int32_t y0, int32_t x1, int32_t y1, Color color)
 {
     float step = 0.001;
     for(float t = 0.0; t < 1.0; t += step)
     {
-        uint32_t x = x0 + lroundf(t * (x1 - x0));
-        uint32_t y = y0 + lroundf(t * (y1 - y0));
+        int32_t x = x0 + lroundf(t * (x1 - x0));
+        int32_t y = y0 + lroundf(t * (y1 - y0));
 
-        if (x >= WIDTH || y >= HEIGHT)
-            continue;
+        if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT) continue;
 
-        uint32_t index = gooParsePixel(x, y) * 3;
-        surface[index + 0] = color.r;
-        surface[index + 1] = color.g;
-        surface[index + 2] = color.b;
+        gooPutPixel(surface, x, y, color); 
     }
 }
-
 
 #endif // GOO_C_
 
--- src/main.c
+++ src/main.c
@@ -5,21 +5,31 @@
 #include "stb_image_write.h"
 
 #include "goo.c"
+#include "obj.h"
 
 int main(void) {
-    uint8_t* image = malloc(HEIGHT * WIDTH * 3);
-    if(!image) {
+    uint8_t* image = malloc(HEIGHT * WIDTH * 4);
+    if(!image)
+    {
         fprintf(stderr, "Failed to allocate memory for the image\n");
         return -1;
     }
-    Color black = {0, 0, 0};
-    Color red = {255, 0, 0};
+    // welp blessings of using .sh file as a build system
+    loadOBJ("../src/assets/untitled.obj");
+    printf("Loaded file assets/untitled.obj with triangle count: %d\n", triangle_count);
 
-    gooFill(image, black);
-    gooDrawLineNaive(image, 0, 0, 300, 300, red);
-    gooDrawLineNaive(image, 393, 300, 500, 432, red);
+    gooFill(image, GRUVBOX_BG);
 
-    if(!stbi_write_png("out.png", WIDTH, HEIGHT, 3, image, WIDTH * 3))
+    gooDrawLineNaive(image, WIDTH/2, HEIGHT/2, WIDTH, HEIGHT, GRUVBOX_RED);
+    gooDrawLineNaive(image, WIDTH/2, HEIGHT/2, WIDTH/2, HEIGHT, GRUVBOX_BLUE);
+    gooDrawLineNaive(image, WIDTH/2, HEIGHT/2, WIDTH, HEIGHT/2, GRUVBOX_GREEN);
+    gooDrawLineNaive(image, WIDTH/2, HEIGHT/2, 0, 0, GRUVBOX_ORANGE);
+    gooDrawLineNaive(image, WIDTH/2, HEIGHT/2, WIDTH/2, 0, GRUVBOX_PURPLE);
+    gooDrawLineNaive(image, WIDTH/2, HEIGHT/2, WIDTH, 0, GRUVBOX_YELLOW);
+    gooDrawLineNaive(image, WIDTH/2, HEIGHT/2, 0, HEIGHT/2, GRUVBOX_AQUA);
+    gooDrawLineNaive(image, WIDTH/2, HEIGHT/2, 0, HEIGHT, GRUVBOX_FG);
+
+    if(!stbi_write_png("out.png", WIDTH, HEIGHT, 4, image, WIDTH * 4))
     {
         fprintf(stderr, "Failed to write image to disk\n");
         return -2;
